a84d0c33423fbcef278c9a7142985596bfb6ab47
Vidishaa Deo <vidishaa2709deo@gmail.com>
2026-08-20 17:43:00 +0530

    Time: 0 ms (100%), Space: 43.1 MB (83.36%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        String prefix = strs[0];
+        for(int i = 1; i <= strs.length - 1; i++) {
+            while(!strs[i].startsWith(prefix)) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if(prefix.isEmpty())
+                    return "";
+            }
+        }
+        return prefix;
+    }
+}
