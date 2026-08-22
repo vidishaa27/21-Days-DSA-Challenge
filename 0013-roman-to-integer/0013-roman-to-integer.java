2de7c23b81666a33d61353e81ffad71b6d3d7a44
Vidishaa Deo <vidishaa2709deo@gmail.com>
2026-08-22 19:19:33 +0530

    Time: 3 ms (77.47%), Space: 46.3 MB (75.1%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,16 +1,26 @@
 class Solution {
     public int romanToInt(String s) {
-        Map<Character, Integer> map = Map.of('I', 1, 'V', 5, 'X', 10, 'L', 50, 'C', 100, 'D', 500, 'M', 1000);
         int result = 0;
-        for(int i = 0; i < s.length(); i++) {
-            int curr = map.get(s.charAt(i));
-            int next = (i + 1 < s.length())? map.get(s.charAt(i + 1)) : 0;
-
-            if(curr < next)
-                result -= curr;
-            else
-                result += curr; 
+        for (int i = 0; i < s.length(); i++) {
+            int current = value(s.charAt(i));
+            if (i + 1 < s.length() && current < value(s.charAt(i + 1))) {
+                result -= current;
+            } else {
+                result += current;
+            }
         }
         return result;
+    }
+    private int value(char c) {
+        switch (c) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+        }
+        return 0;
     }
 }
